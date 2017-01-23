4119d13d995a4213da9e62601bfd0e6ace678113
AdhanIsCuber <AdhanIsCuber@gmail.com>
2017-01-23 22:56:50 +0700

    Commit 3 : Marker Map and Parsing data using Volley

--- app/src/main/java/id/adhaniscuber/parkiryuk/MainActivity.java
+++ app/src/main/java/id/adhaniscuber/parkiryuk/MainActivity.java
@@ -7,15 +7,27 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import br.com.bloder.magic.view.MagicButton;
 import id.adhaniscuber.parkiryuk.multidex.AppController.R;
@@ -97,5 +109,55 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     private void sendRequestArray() {
+
+        String url = "http://parkiryuk.pe.hu/read_alldata.php";
+        loading = ProgressDialog.show(this, "Mohon tunggu...", "Mengambil data...", false, false);
+
+        JsonArrayRequest req = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+
+                        try {
+                            for (int park = 0; park < response.length(); park++){
+                                JSONObject lokasi = (JSONObject) response.get(park);
+
+                                String sNama = lokasi.getString("nama");
+                                String sLat = lokasi.getString("latitude");
+                                String sLong = lokasi.getString("longitude");
+
+                                //Toast.makeText(MainActivity.this, "" + sNama + sLat + sLong, Toast.LENGTH_SHORT).show();
+
+                                Double flat = Double.parseDouble(sLat);
+                                Double flong = Double.parseDouble(sLong);
+                                LatLng fixlok = new LatLng(flat, flong);
+
+                                mMap.addMarker(new MarkerOptions()
+                                        .title(sNama)
+                                        .position(fixlok)
+                                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_markerpy)));
+
+                                //Toast.makeText(MainActivity.this, "" + fixlok, Toast.LENGTH_SHORT).show();
+
+                                mMap.animateCamera(CameraUpdateFactory.newLatLng(fixlok));
+                                mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(getApplicationContext(),"Errors : " + e.getMessage(),Toast.LENGTH_LONG).show();
+                        }
+                        loading.dismiss();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error : " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+                loading.dismiss();
+            }
+        });
+        AppController.getInstance().addToRequestQueue(req);
     }
 }
